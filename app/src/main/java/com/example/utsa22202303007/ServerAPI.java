872fc9cbc8c7932fd88c69d66417b0c5ee54e11f
extobil <candratrikuncoro3@gmail.com>
2025-06-28 20:05:17 +0700

    perubahan

--- app/src/main/java/com/example/utsa22202303007/ServerAPI.java
+++ app/src/main/java/com/example/utsa22202303007/ServerAPI.java
@@ -11,20 +11,17 @@ public class ServerAPI {
     public static String BASE_URL="http://candratri.my.id/androidstudio/";
     public static String IMAGE_BASE_URL= BASE_URL + "/img/";
 
-//    public static String RajaOngkir_Province="https://api.rajaongkir.com/starter/province";
-//    public static String RajaOngkir_Kota="https://api.rajaongkir.com/starter/city?province=";
-
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
         if (retrofit == null) {
             Gson gson = new GsonBuilder()
-                    .setLenient() // Tambahkan ini untuk mengatasi JSON malformed
+                    .setLenient()
                     .create();
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create(gson)) // Gunakan Gson yang sudah di-set lenient
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
